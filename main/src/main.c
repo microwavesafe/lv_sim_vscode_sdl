69befd549a22f015ef80a83bd89ca54b1c0784a7
Mariotaku <mariotaku.lee@gmail.com>
2021-09-01 14:59:06 +0900

    Using custom tick function

--- main/src/main.c
+++ main/src/main.c
@@ -33,7 +33,6 @@
  *  STATIC PROTOTYPES
  **********************/
 static void hal_init(void);
-static int tick_thread(void *data);
 
 /**********************
  *  STATIC VARIABLES
@@ -124,10 +123,6 @@ static void hal_init(void)
 {
   /* Use the 'monitor' driver which creates window on PC's monitor to simulate a display*/
   monitor_init();
-  /* Tick init.
-   * You have to call 'lv_tick_inc()' in periodically to inform LittelvGL about
-   * how much time were elapsed Create an SDL thread to do this*/
-  SDL_CreateThread(tick_thread, "tick", NULL);
   
   lv_disp_t * disp =  lv_disp_get_default();
 
@@ -169,20 +164,4 @@ static void hal_init(void)
   lv_obj_t * cursor_obj = lv_img_create(lv_scr_act()); /*Create an image object for the cursor */
   lv_img_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
   lv_indev_set_cursor(mouse_indev, cursor_obj);             /*Connect the image  object to the driver*/
-}
-
-/**
- * A task to measure the elapsed time for LVGL
- * @param data unused
- * @return never return
- */
-static int tick_thread(void *data) {
-  (void)data;
-
-  while(1) {
-    SDL_Delay(5);
-    lv_tick_inc(5); /*Tell LittelvGL that 5 milliseconds were elapsed*/
-  }
-
-  return 0;
 }
